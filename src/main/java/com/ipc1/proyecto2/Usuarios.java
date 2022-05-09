3beba71080cef7a7d6b25e752632a398b29555f0
BenjaminBritoSantiago <jacintobrito201531237@cunoc.edu.gt>
2022-05-09 13:15:24 -0600

    se agregan los manuales

--- src/main/java/com/ipc1/proyecto2/Usuarios.java
+++ src/main/java/com/ipc1/proyecto2/Usuarios.java
@@ -17,11 +17,6 @@ public class Usuarios {
     
     private int id;
     private String nombre;
-    private int totalParJugo;
-    private int totalParGano;
-    private int totalParPerdio;
-    private int totalMovRealizo;
-    private int promMovRealizo;
     private int[][]  movTiempPatida = new int [10][2];
     
     
@@ -62,21 +57,8 @@ public class Usuarios {
     public Usuarios(int id, String nombre) {
         this.id = id;
         this.nombre = nombre;
-        this.totalParJugo = 0;
-        this.totalParGano = 0;
-        this.totalParPerdio = 0;
-        this.totalMovRealizo = 0;
     }
 
-    public Usuarios(int id, String nombre, int totalParJugo, int totalParGano, int totalParPerdio, int totalMovRealizo, int promMovRealizo) {
-        this.id = id;
-        this.nombre = nombre;
-        this.totalParJugo = totalParJugo;
-        this.totalParGano = totalParGano;
-        this.totalParPerdio = totalParPerdio;
-        this.totalMovRealizo = totalMovRealizo;
-        this.promMovRealizo = promMovRealizo;
-    }
     
     //REPORTES;
 
@@ -295,49 +277,6 @@ public class Usuarios {
 
     public void setNombre(String nombre) {
         this.nombre = nombre;
-    }
-
-    public int getTotalParJugo() {
-        return totalParJugo;
-    }
-
-    public void setTotalParJugo(int totalParJugo) {
-        this.totalParJugo = totalParJugo;
-    }
-
-    public int getTotalParGano() {
-        return totalParGano;
-    }
-
-    public void setTotalParGano(int totalParGano) {
-        this.totalParGano = totalParGano;
-    }
-
-    public int getTotalParPerdio() {
-        return totalParPerdio;
-    }
-
-    public void setTotalParPerdio(int totalParPerdio) {
-        this.totalParPerdio = totalParPerdio;
-    }
-
-    public int getTotalMovRealizo() {
-        return totalMovRealizo;
-    }
-
-    public void setTotalMovRealizo(int totalMovRealizo) {
-        this.totalMovRealizo = totalMovRealizo;
-    }
-
-    public int getPromMovRealizo() {
-        if(totalParJugo>0){
-            return ( totalMovRealizo/totalParJugo );
-        }
-        return 0;
-    }
-
-    public void setPromMovRealizo(int promMovRealizo) {
-        this.promMovRealizo = promMovRealizo;
     }
 
     public int[][] getMovTiempPatida() {
